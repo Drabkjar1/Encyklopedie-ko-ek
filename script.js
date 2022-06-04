a93fdeb6811245b08c71daf18e717a8bb59989da
Drabkjar1 <jaroslava.drabkova@gmail.com>
2022-06-04 16:18:13 +0200

    opravahamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.querySelector('#menu-btn').addEventListener('click', () => {
   document.querySelector('#menu').classList.toggle('menu__closed');
+  document.querySelector('#menu-btn').classList.toggle('menu-btn-closed');
+  document
+    .querySelector('.header_content')
+    .classList.toggle('header_content_nonvisible');
 });
